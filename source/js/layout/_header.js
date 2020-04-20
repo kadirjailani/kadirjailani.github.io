556d5dbd4008f33829deb3cd5181fd2a0d6369fe
Kadir Jailani <31124347+kadirjailani@users.noreply.github.com>
2020-04-20 21:32:03 +0800

    publish items

--- source/js/layout/_header.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    console.log( "i'm ready from imported file!" );
-});
--- source/js/main.js
+++
@@ -1,4 +0,0 @@
-/* import other js file here */
-import "./layout/_header.js";
-
-console.log("This is main js compile file!");
